f8fcdd9342dd83b2b0aeb04aaf02ca54e9e6328e
alivabernali <55328178+alivabernali@users.noreply.github.com>
2023-04-01 22:28:57 -0500

    Update moontunes.js

--- moontunes.js
+++ moontunes.js
@@ -1,47 +1,121 @@
-function playWaves(){
-    var myAudio = document.getElementById("waves");
-    myAudio.volume = 0.1;
+function rainVolume() {
+    var myAudio = document.getElementsByTagName("rain");
+    console.log(document.getElementById("volume1").value)
+    myAudio.volume = document.getElementById("volume1").value;
+    playRain2();
 
-    if (myAudio.paused) {
-        myAudio.play();
-    }     
-    else {
-        myAudio.pause()
-    }   
+    if (!myAudio.paused) {
+        console.log("hi!")
+        myAudio.volume = document.getElementById("volume1").value;
+    }
 }
 
 function playRain(){
-    var myAudio = document.getElementById("rain");
-    myAudio.volume = 0.1;
-
+    var myAudio = document.getElementById("rain"); 
+    myAudio.volume = document.getElementById("volume1").value;  
+    
     if (myAudio.paused) {
         myAudio.play();
     }     
     else {
         myAudio.pause()
     }   
+}
+
+function playRain2(){
+    var myAudio = document.getElementById("rain"); 
+    myAudio.volume = document.getElementById("volume1").value;  
+    
+    console.log(myAudio.volume) 
+    myAudio.play();     
+}
+
+function wavesVolume() {
+    var myAudio = document.getElementsByTagName("waves");
+    console.log(document.getElementById("volume2").value)
+    myAudio.volume = document.getElementById("volume2").value;
+    playWaves2();
+
+    if (!myAudio.paused) {
+        myAudio.volume = document.getElementById("volume1").value;
+    }
+}
+
+function playWaves(){
+    var myAudio = document.getElementById("waves"); 
+    myAudio.volume = document.getElementById("volume2").value;  
+    
+    if (myAudio.paused) {
+        myAudio.play();
+    }     
+    else {
+        myAudio.pause()
+    }   
+}
+
+function playWaves2(){
+    var myAudio = document.getElementById("waves"); 
+    myAudio.volume = document.getElementById("volume2").value;  
+    
+    myAudio.play();     
+}
+
+function forestVolume() {
+    var myAudio = document.getElementsByTagName("forest");
+    console.log(document.getElementById("volume4").value)
+    myAudio.volume = document.getElementById("volume4").value;
+    playForest2();
+
+    if (!myAudio.paused) {
+        myAudio.volume = document.getElementById("volume4").value;
+    }
 }
 
 function playForest(){
-    var myAudio = document.getElementById("forest");
-    myAudio.volume = 0.2;
-
+    var myAudio = document.getElementById("forest"); 
+    myAudio.volume = document.getElementById("volume4").value;  
+    
     if (myAudio.paused) {
         myAudio.play();
     }     
     else {
         myAudio.pause()
-    }   
+    }    
+}
+
+function playForest2(){
+    var myAudio = document.getElementById("forest"); 
+    myAudio.volume = document.getElementById("volume4").value;  
+    
+    myAudio.play();     
+}
+
+function bubblesVolume() {
+    var myAudio = document.getElementsByTagName("bubbles");
+    console.log(document.getElementById("volume3").value)
+    myAudio.volume = document.getElementById("volume3").value;
+    playBubbles2();
+
+    if (!myAudio.paused) {
+        myAudio.volume = document.getElementById("volume3").value;
+    }
 }
 
 function playBubbles(){
-    var myAudio = document.getElementById("bubbles");
-    myAudio.volume = 0.1;
-
+    var myAudio = document.getElementById("bubbles"); 
+    myAudio.volume = document.getElementById("volume3").value;  
+    
     if (myAudio.paused) {
         myAudio.play();
     }     
     else {
         myAudio.pause()
-    }   
+    }    
+}
+
+function playBubbles2(){
+    var myAudio = document.getElementById("bubbles"); 
+    myAudio.volume = document.getElementById("volume3").value;  
+    
+    myAudio.play();     
 }
